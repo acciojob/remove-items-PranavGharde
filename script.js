5a2370103a0228f1386849d99daa4efd6752eb1f
PranavGharde <pranavgharde32@gmail.com>
2025-06-05 19:49:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+window.onload = function(){
+	const select = document.getElementById("colorSelect");
+	const button = document.querySelector('input[type="button"]');
+
+	button.addEventListener("click", function (){
+		const selectedIndex = select.selectedIndex;
+
+		if(selectedIndex !== -1){
+			select.remove(selectedIndex);
+		}
+	});
+}
